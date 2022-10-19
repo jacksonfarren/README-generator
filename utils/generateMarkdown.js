17981f5a8849e843d586de3238982c403f88a81a
Jackson <jacksonfarren@hotmail.com>
2022-10-19 01:13:57 -0700

    File fix and readme fix

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -4,7 +4,7 @@ function renderLicenseBadge(license) {
   if (!license) {
     return ``
   } else {
-    return `![badge](https://img.shields.io/badge/license-${license}-brightgreen)`
+    return `![badge](https://img.shields.io/badge/license-${license}-blue)`
   }
 }
 
